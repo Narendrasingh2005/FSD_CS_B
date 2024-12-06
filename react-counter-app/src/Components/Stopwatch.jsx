01afb09557816712246f4982affbc3f3193c4f96
Narendra Singh <ns1039492@gmail.com>
2024-12-06 09:39:53 +0530

    stopwatch code

--- react-counter-app/src/Components/Stopwatch.jsx
+++ react-counter-app/src/Components/Stopwatch.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const Stopwatch = () => {
+    const [isrunning,setIsrunning]=useState(false)
+    const [time,setTime]=useState(0)
+    function handleStartStop(){
+        setIsrunning((p)=>!p)
+    }
+    function handleReset{
+        setTime(0);
+        clearInterval()
+    }
+    useEffect(()=>{
+        if (isrunning) {
+            const id=setInterval(()=>{
+                setTime((p)=>p+1)
+            },1000)
+            setValidId(id);
+        }
+        return ()=>clearInterval(setValidId)
+    },[isrunning])
 return (
     <div>
     <h1>stop watch App</h1>
+    <h1>Timer</h1>
+    <button onClick={handleStartStop}>{isrunning?'Stop':'Start'}</button>
+    <button onClick={handleReset}>Reset</button>
     </div>
 )
 }
